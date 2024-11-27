82cf11cdb0484af7cd86ce2068b390e0031dbe22
kkirrin <khlebnikov-2016@mail.ru>
2024-11-27 22:05:33 +1000

    feat: добавляет новый tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import { Metadata } from 'next';
-import { Button, Htag, Ptag } from './components';
+import { Button, Htag, Ptag, Tag } from './components';
 
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -32,8 +32,12 @@ export default function Home(): JSX.Element {
 			<Button appearance='ghost' arrow='up'>Button</Button>
 			<Button appearance='primary' arrow='down'>More btn</Button>
 			<Ptag size='xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum optio quod et at quam. Obcaecati voluptas ullam molestias omnis. Obcaecati alias, at esse aliquam minima natus nesciunt molestias hic perferendis?</Ptag>
-
+			<Tag size='medium' color="ghost" href='/'>ТЕГ</Tag>
+			<Tag size='medium' color='gray'>ТЕГ</Tag>
+			<Tag size='medium' color='red'>ТЕГ</Tag>
+			<Tag size='medium' color="green">ТЕГ</Tag>
 		</>
+
 
 	);
 }
