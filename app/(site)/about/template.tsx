103f2fad5e98384aa9aaaf3cf7a7f756467bf664
kkirrin <khlebnikov-2016@mail.ru>
2024-12-01 19:40:32 +1000

    feat: template  для about (не используется пока что)

---
+++ app/(site)/about/template.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function AboutTemplate({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div style={{ border: '1px solid red'}}>
+            {children}
+        </div>
+    )
+}
